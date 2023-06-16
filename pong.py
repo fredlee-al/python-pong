3ac315e2080ae80d805d32bbca3a7421d01c7b88
fredlee-al <fredlee.a.l@gmail.com>
2023-06-16 21:18:05 +0530

    Initial commit

---
+++ pong.py
@@ -0,0 +1,129 @@
+import turtle
+
+wn=turtle.Screen()
+wn.title("Pong by Fred")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=4, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=4, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.shapesize(stretch_wid=3, stretch_len=3)
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 210)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Roboto", 24, "normal"))
+
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "q" )
+wn.onkeypress(paddle_a_down, "a")
+wn.onkeypress(paddle_b_up, "w" )
+wn.onkeypress(paddle_b_down, "s")
+
+
+
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    
+    # Top and bottom
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Roboto", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Roboto", 24, "normal"))
+
+#Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350):
+        if ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+            ball.setx(335)
+            ball.dx *= -1.1  
+            
+
+    if (ball.xcor() < -340 and ball.xcor() > -350):
+        if ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+            ball.setx(-335)
+            ball.dx *= -1.1  
+            
